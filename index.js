544d9f252c26404936268893036633296ad7c3aa
Volker Christian <volker.christian@fh-hagenberg.at>
2020-01-02 11:05:36 +0100

    First Version

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
+
+const directories = source = > fs.readdirSync(source, {
+    withFileTypes: true
+}).reduce((a, c) = > {
+    c.isDirectory() && c.name.endsWith('.skill') && a.push(c.name)
+    return a
+}, [])
+
+const skillDirs = directories('.');
+
+const app = express();
+app.use(bodyParser.json());
+
+const skillEndpoints = {};
+
+skillDirs.forEach(function(skilldir) {
+    skillEndpointName = skilldir.replace('.skill', '');
+
+    console.log("Registering Skillendpoint /" + skillEndpointName);
+
+    skillEndpoints[skillEndpointName] = require('./' + skilldir + '/skill').handler;
+
+    app.post('/' + skillEndpointName, function(req, res) {
+        skillEndpointName = req.url.replace(/\//g, '');
+
+        console.log(`REQUEST++++$ {
+            JSON.stringify(skillEndpointName)
+        }`);
+
+        skillEndpoints[skillEndpointName].invoke(req.body)
+            .then(function(responseBody) {
+            res.json(responseBody);
+        })
+            .
+        catch (function(error) {
+            console.log(error);
+            res.status(500).send('Error during the request');
+        });
+
+        console.log(`RESPONSE++++$ {
+            JSON.stringify(res.body)
+        }`);
+    });
+});
+
+app.listen(8080, function() {
+    console.log('Development endpoint listening on port 8080!');
+});
