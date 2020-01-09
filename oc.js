7aa1252760d7ac2867097b2b1435e3f814c77938
Volker Christian <volker.christian@fh-hagenberg.at>
2020-01-09 15:49:42 +0100

    Added amazon api-endpoint access token handling

--- oc.js
+++ oc.js
@@ -10,7 +10,7 @@ var db;
 
 function handleDisconnect() {
     db = mysql.createConnection({
-//        host: 'proliant.home.vchrist.at',
+        //        host: 'proliant.home.vchrist.at',
         host: '192.168.1.3',
         user: 'wastecalendar',
         password: '!!!SoMaSi01!!!'
@@ -30,7 +30,8 @@ function handleDisconnect() {
     db.query = function(sql, values, cb) {
         db.origQuery(sql, values, function(err, result) {
             if (err) {
-                // handle socket and lower level errors
+                console.log('Error: ' + err.stack);
+                setTimeout(handleDisconnect, 1000);
             }
             cb(err, result);
         });
@@ -83,16 +84,16 @@ app.get('/auth/nextcloud', function(req, res) {
 
 function insertUser(user, cb) {
     console.log('AUTH: Create account for user ' + user.data.user_id);
-    
+
     var sql = 'INSERT INTO wastecalendar.oc_user SET ?';
-    
+
     var ocUser = {
         oc_userid: user.data.user_id,
         oc_accessToken: user.accessToken,
         oc_refreshtoken: user.refreshToken,
         oc_expires: user.expires
     };
-    
+
     db.query(sql, ocUser, function(err, result) {
         if (err) {
             console.error(err.stack);
@@ -107,29 +108,9 @@ function insertUser(user, cb) {
     });
 }
 
-/*
-let sql = `UPDATE todos
-           SET completed = ?
-           WHERE id = ?`;
- 
-let data = [false, 1];
-
-// execute the UPDATE statement
-connection.query(sql, data, (error, results, fields) => {
-  if (error){
-    return console.error(error.message);
-  }
-  console.log('Rows affected:', results.affectedRows);
-});
-
-////////////////////////
-
-connection.query('UPDATE user SET ? WHERE ?', [{ Name: name }, { UserId: userId }])
-*/
-
 function refreshUser(user, cb) {
-    console.log('Refresh Token');
-    
+    console.log('RT: Refreshing token for user ' + user.data.user_id);
+
     user.refresh().then(function(updatedUser) {
         console.log('AccessToken: ' + updatedUser.accessToken);
         console.log('RefreshToken: ' + updatedUser.refreshToken);
@@ -147,9 +128,9 @@ function refreshUser(user, cb) {
                 oc_userid: updatedUser.data.user_id
             }
         ];
-        
+
         var sql_UpdateToken = 'UPDATE wastecalendar.oc_user SET ? WHERE ?';
-        
+
         db.query(sql_UpdateToken, updatedToken, function(err, result) {
             if (err) {
                 console.error(err.stack);
@@ -157,34 +138,12 @@ function refreshUser(user, cb) {
                 result.end();
                 return;
             }
-            console.log(result.affectedRows + ' record updated ' + util.inspect(result));
+            console.log(result.affectedRows + ' record updated');
 
             if (cb) {
                 cb(updatedUser);
             }
         });
-        /*
-        var sql = `UPDATE wastecalendar.oc_user
-        SET
-        oc_accesstoken = ${db.escape(updatedUser.accessToken)},
-            oc_refreshtoken = ${db.escape(updatedUser.refreshToken)},
-            oc_expires = ${db.escape(updatedUser.expires)}
-        WHERE
-        oc_userid = ${db.escape(updatedUser.data.user_id)}`;
-
-        db.query(sql, function(err, result) {
-            if (err) {
-                console.error(err.stack);
-                result.statusCode = 500;
-                result.end();
-                return;
-            }
-            console.log(result.affectedRows + ' record updated ' + util.inspect(result));
-
-            if (cb) {
-                cb(updatedUser);
-            }
-        });*/
     });
 }
 
@@ -196,7 +155,9 @@ app.get('/auth/nextcloud/callback', function(req, res) {
     nextcloudAuth.code.getToken(req.originalUrl).then(function(user) {
         console.log(user);
 
-        var sql = `SELECT * FROM wastecalendar.oc_user WHERE oc_userid = ${db.escape(user.data.user_id)}`;
+        var sql = `SELECT * FROM wastecalendar.oc_user WHERE oc_userid = $ {
+            db.escape(user.data.user_id)
+        }`;
 
         db.query(sql, function(err, result) {
             if (err) {
@@ -208,7 +169,9 @@ app.get('/auth/nextcloud/callback', function(req, res) {
             console.log(result.affectedRows + ' records found ' + util.inspect(result));
 
             if (result && result.length) {
-                sql = `DELETE FROM wastecalendar.oc_user WHERE oc_userid = ${db.escape(user.data.user_id)}`;
+                sql = `DELETE FROM wastecalendar.oc_user WHERE oc_userid = $ {
+                    db.escape(user.data.user_id)
+                }`;
                 db.query(sql, function(err, result) {
                     if (err) {
                         console.error(err.stack);
@@ -217,7 +180,7 @@ app.get('/auth/nextcloud/callback', function(req, res) {
                         return;
                     }
                     console.log(result.affectedRows + ' records updated ' + util.inspect(result));
-                    
+
                     insertUser(user, function(user) {
                         refreshUser(user, function(updatedUser) {
                             return res.send(updatedUser.accessToken);
@@ -244,7 +207,7 @@ Date.unixTime = function() {
 };
 
 function processCalendar(user, cb) {
-    console.log('PC Calendar');
+    console.log('PC: Calendar');
     var rec = user.sign({
         url: 'https://cloud.vchrist.at/remote.php/dav/calendars/' + user.data.user_id + '/mllabfuhr/?export' + '&expand=1' + '&start=' + Date.unixTime() + '&end=' + Date.unixTime() + 3600 * 24,
         headers: {
@@ -254,14 +217,10 @@ function processCalendar(user, cb) {
 
     request.get(rec, function(error, response, body) {
         var iCalData = JSON.parse(body);
-
-        //var iCalData = ICAL.parse(body);
-        //console.log('Call Response: ' + JSON.stringify(iCalData, null, 4));
-        //console.log('Call Status: ' + response.statusCode);
-
         var comp = new ICAL.Component(iCalData);
         var vevent = comp.getFirstSubcomponent('vevent');
         var event = new ICAL.Event(vevent);
+
         var str = '';
         if (event.startDate) {
             str = 'Event Summary: ' + event.summary + '\nLocale Start: ' + event.startDate.toJSDate() + '\nLocale End: ' + event.endDate.toJSDate();
@@ -291,15 +250,18 @@ app.get('/test', function(req, res) {
         }
 
         result.forEach(function(oc_user) {
-            console.log('PC: Processing user ' + oc_user.oc_userid + ':');
-            
-            var user = nextcloudAuth.createToken(oc_user.oc_accesstoken,
-                                                 oc_user.oc_refreshtoken,
-                                                 'bearer',
-                                                 {
-                                                    user_id: oc_user.oc_userid,
-                                                    expires_in: oc_user.oc_expires.toUnixTime() - Date.unixTime() - 600
-                                                 });
+            console.log('PC: Processing user ' + oc_user.oc_userid);
+
+            var tokenData = {
+                access_token: oc_user.oc_accesstoken,
+                refresh_token: oc_user.oc_refreshtoken,
+                token_type: 'bearer',
+                user_id: oc_user.oc_userid,
+                expires_in: oc_user.oc_expires.toUnixTime() - Date.unixTime() - 600
+            };
+
+            var user = nextcloudAuth.createToken(tokenData);
+
             if (user.expired()) {
                 refreshUser(user, function(updatedUser) {
                     processCalendar(updatedUser, function(body) {
@@ -312,6 +274,113 @@ app.get('/test', function(req, res) {
                 });
             }
         });
+    });
+});
+
+function refreshAmzProactiveEndpointToken(cb) {
+    var options = {
+        'method': 'POST',
+        'url': 'https://api.amazon.com/auth/o2/token',
+        'headers': {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        form: {
+            'grant_type': 'client_credentials',
+            'client_id': 'amzn1.application-oa2-client.c1494a447d77405883037efdc06baad6',
+            'client_secret': '07c7affba53c9d2632186cff30c678d5ed243efc6140436c533f2eac32e8dd11',
+            'scope': 'alexa::proactive_events'
+        }
+    };
+
+    request.post(options, function(error, response, body) {
+        cb(error, response, body);
+    });
+}
+
+const skillid = 'amzn1.ask.skill.5119403b-f6c6-45f8-bd7e-87787e6f5da2';
+
+function getAmzProactiveEndpointAccessToken(amz_skillid, cb) {
+    var sql = 'SELECT * FROM wastecalendar.amz_endpoint WHERE amzep_skillid = ' + db.escape(amz_skillid);
+
+    console.log('AMZ: Looking for access-token for skill \'' + amz_skillid + '\'');
+
+    db.query(sql, function(err, result) {
+        if (!(result && result.length)) {
+            console.log('AMZ: No access token found for skill \'' + amz_skillid + '\' ... retrieving');
+            refreshAmzProactiveEndpointToken(function(error, response, body) {
+                if (error) {
+                    throw new Error(error);
+                }
+                body = JSON.parse(response.body);
+                console.log('AMZ: Got new access token for skill \'' + amz_skillid + '\': ' + body.expires_in + ' - ' + body.access_token);
+
+                var amzEndpointToken = {
+                    amzep_skillid: amz_skillid,
+                    amzep_accesstoken: body.access_token,
+                    amzep_expires: new Date((Date.unixTime() + body.expires_in - 600) * 1000)
+                };
+
+                var sql = 'INSERT INTO wastecalendar.amz_endpoint SET ?';
+                
+                db.query(sql, amzEndpointToken, function(err, result) {
+                    if (err) {
+                        throw new Error(err);
+                    }
+                    console.log(result.affectedRows + ' records inserted ');
+
+                    cb({expires: body.expires_in, token: body.access_token});
+                });
+            });
+        } else {
+            console.log('AMZ: Access token found for skill \'' + amz_skillid + '\': ' + result[0].amzep_expires + ' - ' + result[0].amzep_accesstoken);
+            if (result[0].amzep_expires.toUnixTime() - Date.unixTime() < 600) {
+                // Token expired
+                console.log('AMZ: Token expired. Refreshing ...');
+
+                refreshAmzProactiveEndpointToken(function(error, response, body) {
+                    if (error) {
+                        throw new Error(error);
+                    }
+                    body = JSON.parse(response.body);
+                    var expires = new Date((Date.unixTime() + body.expires_in) * 1000);
+                    
+                    console.log('AMZ: Got new access token for skill \'' + amz_skillid + '\': ' + expires + ' - ' + body.access_token);
+                    
+                    var amzUpdatedToken = [
+                        // new values
+                        {
+                            amzep_accesstoken: body.access_token,
+                            amzep_expires: expires
+                        },
+                        // condition
+                        {
+                            amzep_skillid: amz_skillid
+                        }
+                    ];
+
+                    var sql = 'UPDATE wastecalendar.amz_endpoint SET ? WHERE ?';
+
+                    db.query(sql, amzUpdatedToken, function(err, result) {
+                        if (err) {
+                            throw new Error(err);
+                        }
+                        console.log(result.affectedRows + ' records inserted ');
+
+                        cb({expires: expires, token: body.access_token});
+                    });
+                });
+            } else {
+                console.log('AMZ: Token valid');
+                // Token not expired
+                cb({expires: result[0].amzep_expires, token: result[0].amzep_accesstoken});
+            }
+        }
+    });
+}
+
+app.get('/amz', function(req, res) {
+    getAmzProactiveEndpointAccessToken(skillid, function(accessToken) {
+       return res.send('SkillId: ' + skillid + ': ' + '\n\tToken: ' + accessToken.token + '\n\tExpires: ' + accessToken.expires + '\n');
     });
 });
 
